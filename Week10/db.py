995075692dc0a734df457de8cf421a35a30e47b6
omar <m43tol@gmail.com>
2025-03-18 10:17:25 -0400

    lab week11

--- Week10/db.py
+++ Week10/db.py
@@ -1,36 +1,34 @@
 import sqlite3
 import function
+
 db_connection = sqlite3.connect("sqlite.db")
-print (db_connection)
+db_connection.row_factory = sqlite3.Row  
+print(db_connection)
 
 db_cursor = db_connection.cursor()
 print(db_cursor)
 
-query1 = "SELECT * FROM demo"
-db_cursor.execute(query1)
+query_q1 = "SELECT * FROM demo WHERE id > 14"
+db_cursor.execute(query_q1)
+
+names = [row["Name"] for row in db_cursor.fetchall()]
+print("Names:", names)
+
 print("Reading 1 row")
-
 row = db_cursor.fetchone()
-print (row)
+print(row)
 
-print("Reading 3 rows")
-#rows = db_cursor.fetchmany(3)
-#for r in rows:
- #   print(r)
-function.query_responder(db_cursor, "fetchall")
-
-    
-
-print("Reading all rows")
-# rows = db_cursor.fetchall()
-# for r in rows:
-#     print(r)
 function.query_responder(db_cursor, "fetchall")
 
 query2 = "INSERT INTO demo (Name, Hint) VALUES ('Michael', 'Murphy')"
 db_cursor.execute(query2)
 db_connection.commit()
-id= int(input("Enter an id: "))
-query3 = "SELECT * FROM demo WHERE ID > ?"
-#db_cursor.execute(query3, (id))
-#function.query_responder(db_cursor, "fetchall")
+
+del_row = int(input("Enter an id to delete rows with id < this value: "))
+query_q2 = "DELETE FROM demo WHERE id < ?"
+db_cursor.execute(query_q2, (del_row,))
+affected_rows = db_cursor.rowcount
+
+print(f"{affected_rows} rows affected. Are you sure you want to continue?")
+
+db_connection.close()
